5d6390a030343376cbabe2665a6f357ca3f4da8f
benwins432233-eng <benwins432233@gmail.com>
2026-05-12 01:08:48 +0100

    Suppression de l'accès au dépôt github des projets présentés

--- src/App.tsx
+++ src/App.tsx
@@ -637,16 +637,6 @@ function ProjectsSection() {
                     >
                       <ExternalLink className="w-5 h-5" />
                     </motion.a>
-                    <motion.a
-                      href={project.githubUrl}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                      whileHover={{ scale: 1.1 }}
-                      whileTap={{ scale: 0.9 }}
-                      className="p-3 rounded-full bg-white text-black"
-                    >
-                      <Github className="w-5 h-5" />
-                    </motion.a>
                   </motion.div>
                 </div>
 
